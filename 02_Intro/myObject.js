f34c9a541d21a3e64f4e8f1aeb10b2f909d95e15
ShubhamKumar37 <sk9818281820@gmail.com>
2025-02-05 15:48:18 +0530

    Add TypeScript and JavaScript examples for user creation and type enforcement

--- 02_Intro/myObject.js
+++ 02_Intro/myObject.js
@@ -1,43 +1,24 @@
-const user = {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var user = {
     name: "Shubham Kumar",
     email: "sk@gmail.com",
     isActive: true,
 };
-
 // Now if i want to pass the email as the object in argument of invoked function then this will cause error which it should be
 // but if i create a another object where i mention a additional parameter email and then pass it to the argument then it will not raise any error
-let myObj = {
+var myObj = {
     name: "shubham",
     isPaid: true,
     email: "sk918281820@gmail.com"
 };
-
-function createUser({ name: string, isPaid: boolean }) { }
-
+function createUser(_a) {
+    var string = _a.name, boolean = _a.isPaid;
+}
 // createUser({name: "shubham", isPaid: true});
 // createUser({name: "shubham", isPaid: truem, email: "sk@gmail.com"}); // Here it will cause error
 createUser(myObj); // This will not generate any error, if the parameter of function property are not there in object then it will cause error
-
-
-
-function createCourse(): { name: string, price: number } {
+function createCourse() {
     return { name: "nitin", price: 200 };
 }
-
 createUser({ name: "shubham kumar", isPaid: true });
-
-
-type User = {
-    name: string,
-    isActive: boolean,
-    email: string
-};
-
-function makeUser(user: User): User {
-    return { name: "", email: "", isActive: false };
-}
-
-// makeUser({ name: "", email: "", isActive: false, course: "Hello" }); // This cause proble because the course property is not mention in type
-makeUser({ name: "", email: "", isActive: false });
-
-export { };
